56ea570db34cb58873247ff160d4d8c34b147b10
ashukla333 <amanshukla3747@gmail.com>
2024-08-15 14:57:11 +0530

    changes done

--- utils/feature.js
+++ utils/feature.js
@@ -14,15 +14,19 @@
 
 import jwt from 'jsonwebtoken';
 
+// export const generateCookie = (email) => {
+//   // Ensure the secret key is set
+//   if (!process.env.JsonWEBToken) {
+//     throw new Error('JWT secret key is not defined in environment variables');
+//   }
+
+//   // Generate JWT token
+//   const token = jwt.sign({ email }, process.env.JsonWEBToken, { expiresIn: '24h' });
+
+//   // Return the generated token
+//   return token;
+// };
+
 export const generateCookie = (email) => {
-  // Ensure the secret key is set
-  if (!process.env.JsonWEBToken) {
-    throw new Error('JWT secret key is not defined in environment variables');
-  }
-
-  // Generate JWT token
-  const token = jwt.sign({ email }, process.env.JsonWEBToken, { expiresIn: '24h' });
-
-  // Return the generated token
-  return token;
+    return jwt.sign({ email }, process.env.JsonWEBToken, { expiresIn: '24h' });
 };
